4c27b8b4d0155b0be6bed91e215a7836069b6226
9527A <44466160+9527A@users.noreply.github.com>
2018-11-04 21:56:16 +0800

    Add files via upload

--- crawler_main.py
+++ crawler_main.py
@@ -8,22 +8,25 @@ root_s = win_s/2-350
 root_h = win_h/2-100
 window.geometry('%dx%d+%d+%d' % (root_s,root_h,500,250))
 
-l_1 = Label(window, text='user name')
-l_1.grid(row=1, column=0, sticky=N+E+S+W)
+# l_1 = Label(window, text='user name')
+# l_1.grid(row=1, column=0, sticky=N+E+S+W)
 
-var_1 = StringVar()
-e_1=Entry(window, textvariable=var_1)
-e_1.grid(row=1, column=1, sticky=N+E+S+W)
+# var_1 = StringVar()
+# e_1=Entry(window, textvariable=var_1)
+# e_1.grid(row=1, column=1, sticky=N+E+S+W)
 
-l_2 = Label(window, text='passworld')
-l_2.grid(row=2, column=0, sticky=N+E+S+W)
+# l_2 = Label(window, text='passworld')
+# l_2.grid(row=2, column=0, sticky=N+E+S+W)
 
-var_2 = StringVar()
-e_2 = Entry(window, textvariable=var_2, show='*')
-e_2.grid(row=2, column=1, sticky=N+E+S+W)
+# var_2 = StringVar()
+# e_2 = Entry(window, textvariable=var_2, show='*')
+# e_2.grid(row=2, column=1, sticky=N+E+S+W)
 
-Button(window, text='login').grid(row=3, column=0, sticky=N+S)
-Button(window, text='sign up').grid(row=3, column=1, sticky=N+S)
+Button(window, text='开始爬取').grid(row=3, column=0, sticky=W, pady=3)
+Button(window, text='显示数据').grid(row=3, column=1, sticky=W, pady=3)
 
+t = Text(window)
+t.insert(1.0, 'hello')
+t.grid(row=4, column=0, columnspan=2, padx=1, pady=2)
 
 window.mainloop()
