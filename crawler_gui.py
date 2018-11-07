f5b40a0db40dafd06dcaa4181ec968b6fee62984
9527A <44466160+9527A@users.noreply.github.com>
2018-11-08 00:58:53 +0800

    Add files via upload

--- crawler_gui.py
+++ crawler_gui.py
@@ -1,4 +1,17 @@
 from tkinter import *
+import crawler_downloader
+
+downloader = []
+
+def button_download():
+    global downloader
+    downloader = crawler_downloader.download()
+    # t.insert(INSERT, downloader)
+    t.insert(INSERT, '爬取完成')
+
+def button_show():
+    t.delete(0.0,END)
+    t.insert(INSERT, downloader)
 
 window = Tk()
 window.title("hello world")
@@ -8,11 +21,10 @@ root_s = win_s/2-350
 root_h = win_h/2-100
 window.geometry('%dx%d+%d+%d' % (root_s,root_h,500,250))#使界面显示位置近似居中
 
-Button(window, text='开始爬取').grid(row=3, column=0, sticky=W, pady=3)
-Button(window, text='显示数据').grid(row=3, column=1, sticky=W, pady=3)
-
 t = Text(window)
-t.insert(1.0, 'hello')
 t.grid(row=4, column=0, columnspan=2, padx=1, pady=2)
+
+Button(window, text='开始爬取', command=lambda:button_download()).grid(row=3, column=0, sticky=W, pady=3)
+Button(window, text='显示数据', command=lambda:button_show()).grid(row=3, column=1, sticky=W, pady=3)
 
 window.mainloop()
